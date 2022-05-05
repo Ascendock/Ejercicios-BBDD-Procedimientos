8a33f86ee508184f4ce9cd93e45c580cdd28de62
Bruno DC <bdezac@gmail.com>
2022-05-06 00:12:37 +0200

    Update Ej_4_Moodle_Procedimientos_SIN_PARAMETROS.sql

--- 001_Procedimientos_sin_parametros/Ej_4_Moodle_Procedimientos_SIN_PARAMETROS.sql
+++ 001_Procedimientos_sin_parametros/Ej_4_Moodle_Procedimientos_SIN_PARAMETROS.sql
@@ -8,11 +8,10 @@ CREATE PROCEDURE atracciones_getSumEspectGanancByAtracc()
 COMMENT "Devuelve todas las ganancias y espectadores de la atraccion el gran felino"
 BEGIN
 
-	SELECT DISTINCT ATRACCIONES.nombre,SUM(ATRACCIONES.ganancias) as Gan_totales,
-					SUM(ATRACCION_DIA.num_espectadores) as espect_totales
+    SELECT DISTINCT ATRACCIONES.nombre,SUM(ATRACCIONES.ganancias) as Gan_totales,SUM(ATRACCION_DIA.num_espectadores) as espect_totales
     FROM ATRACCIONES INNER JOIN ATRACCION_DIA
-					ON ATRACCIONES.nombre = ATRACCION_DIA.nombre_atraccion
-	WHERE ATRACCIONES.nombre = "El Gran Felino"
+			ON ATRACCIONES.nombre = ATRACCION_DIA.nombre_atraccion
+    WHERE ATRACCIONES.nombre = "El Gran Felino"
     ORDER BY ATRACCIONES.ganancias;
 
 END$$
