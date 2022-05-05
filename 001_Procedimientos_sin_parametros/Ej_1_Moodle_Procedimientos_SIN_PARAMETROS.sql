f305a2fc1be129bde4407c313922fcb8e384cb2a
Bruno DC <bdezac@gmail.com>
2022-05-05 23:45:32 +0200

    Update Ej_1_Moodle_Procedimientos_SIN_PARAMETROS.sql

--- 001_Procedimientos_sin_parametros/Ej_1_Moodle_Procedimientos_SIN_PARAMETROS.sql
+++ 001_Procedimientos_sin_parametros/Ej_1_Moodle_Procedimientos_SIN_PARAMETROS.sql
@@ -8,11 +8,10 @@ CREATE PROCEDURE ANIMALES_getListNome()
 COMMENT "Crea un procedimiento que muestra los nombres de los animales 
 ordenados por peso en orden descendente"
 BEGIN
-		SELECT ANIMALES.nombre, ANIMALES.peso
+	SELECT ANIMALES.nombre, ANIMALES.peso
         FROM ANIMALES
         ORDER BY ANIMALES.peso DESC;
 
 END$$
-DELIMITER ;
 
 CALL ANIMALES_getListNome;
